712f18df74d529656a8d1593201fa1d9c157b677
Iamthelaw <robotehnik@me.com>
2015-12-18 17:38:07 +0300

    tests improved

--- sitemap_extractor/__init__.py
+++ sitemap_extractor/__init__.py
@@ -1,1 +1,1 @@
-from sitemap import Sitemap, get_http_content
+from .sitemap import Sitemap, get_http_content
--- test_sitemap.py
+++ test_sitemap.py
@@ -1,34 +1,29 @@
 import unittest
 import types
-from requests import URLRequired, RequestException
+from requests import RequestException
 from sitemap_extractor import Sitemap, get_http_content
 
 S = Sitemap()
-
-with open("sitemap_for_test_v1.xml", "r") as f:
-    test_xml_1 = S.get_elements_from_xml(f.read())
-with open("sitemap_for_test_v2.xml", "r") as f:
-    test_xml_2 = S.get_elements_from_xml(f.read())
 
 
 class TestGetHttpContent(unittest.TestCase):
     def test_incorrect_url(self):
         try:
             _ = get_http_content("abc")
-        except URLRequired:
+        except SystemExit:
             _ = False
         self.assertFalse(_)
 
     def test_bad_status_code(self):
         try:
-            _ = get_http_content("http://yandex.ru")
+            _ = get_http_content("http://yandex.ru/404/")
         except RequestException:
             _ = False
         self.assertFalse(_)
 
     def test_returns_xml_content(self):
         try:
-            content = get_http_content("http://tavil.ru")
+            content = get_http_content("http://tavil.ru/sitemap.xml")
         except Exception as e:
             content = False
         self.assertTrue(content)
@@ -39,25 +34,18 @@ class TestGetHttpContent(unittest.TestCase):
 
 class TestGetElementsFromXml(unittest.TestCase):
     def test_returned_content(self):
-        result = S.get_elements_from_xml("http://tavil.ru")
+        result = S.get_elements_from_xml("http://tavil.ru/sitemap.xml")
         self.assertTrue(isinstance(result, types.GeneratorType))
 
 
 class TestParseXML(unittest.TestCase):
-    def SetUp(self):
-        self.parse_xml_result_1 = S.parse_xml(test_xml_1)
-        self.parse_xml_result_2 = S.parse_xml(test_xml_2)
+    def setUp(self):
+        self.result = S.parse_xml(get_http_content('http://tavil.ru'))
 
     def test_returns_generator(self):
         self.assertTrue(
-            isinstance(self.parse_xml_result_1, types.GeneratorType)
+            isinstance(self.result, types.GeneratorType)
         )
-
-    def test_xml_links_not_in_result(self):
-        self.assertTrue(".xml" not in self.parse_xml_result_2)
-
-    def test_splitted_sitemap(self):
-        self.assertTrue(".xml" in self.parse_xml_result_1)
 
 if __name__ == "__main__":
     unittest.main()
