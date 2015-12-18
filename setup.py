527596c1bc5f9b8a9cbae8b2b424d5ef1090f8ae
Iamthelaw <robotehnik@me.com>
2015-12-18 12:04:13 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="sitemap extractor",
+    version="0.1a",
+    description="small script for extracting links from website sitemap.xml",
+    url="https://github.com/Iamthelaw/sitemap_extractor",
+    author="Robotehnik",
+    author_email="robotehnik@me.com",
+    license="MIT",
+    packages=["sitemap_extractor"],
+    zip_safe=False
+)
